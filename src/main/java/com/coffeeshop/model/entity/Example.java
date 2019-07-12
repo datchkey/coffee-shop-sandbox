b47dc85354dff0efbc60338dbe0ec78a47bc0bd7
edward <edward.kats@outlook.com>
2019-07-12 12:08:39 +0300

    1. added liquibase configuration

--- src/main/java/com/coffeeshop/model/entity/Example.java
+++ src/main/java/com/coffeeshop/model/entity/Example.java
@@ -2,10 +2,7 @@ package com.coffeeshop.model.entity;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 
 @AllArgsConstructor
@@ -14,14 +11,23 @@ import javax.validation.constraints.NotBlank;
 @Setter
 @Builder
 @Entity
+@Table(name = "example")
 public class Example {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
     private Long id;
 
+    @Column(name = "NAME", nullable = false, length = 50)
     private String name;
+
+    @Column(name = "EMAIL", unique = true, nullable = false, length = 50)
     private String email;
+
+    @Column(name = "PRICE", nullable = false)
     private Integer price;
+
+    @Column(name = "IMAGE_URL", nullable = false)
     private String imageUrl;
 }
